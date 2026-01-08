050c3b52b2e5f6c847afe6c2ec437eecbbcd7c5b
suraj-shrivastav <suraj.shrivastavvv@gmail.com>
2026-01-08 13:16:49 +0530

    env fixed

--- frontend/src/libs/api.js
+++ frontend/src/libs/api.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 import { supabase } from "./supabaseClient";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
+const API_BASE_URL = import.meta.env.VITE_BACKEND_URL || "https://pitchdekh-backend.onrender.com";
+if (API_BASE_URL) {
+    console.log("Base URL is: ", API_BASE_URL)
+} else {
+    console.log("No Url from .env");
+}
 
 export const api = axios.create({
     baseURL: API_BASE_URL,
